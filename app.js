d786e909bff614cbd26ffa1bace3e68d0ed30b04
zlawson13 <zach@executivedsllc.com>
2019-09-13 10:42:51 -0500

    Adding Query Selectors For People

--- app.js
+++ app.js
@@ -8,21 +8,34 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 
 document.querySelector('.dice').stlye.display = 'none';
+
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+   
+    //Random Number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    //Display The result
+    var diceDOM = document.querySelector('.dice');
+    diceDom.style.display='block';
+    diceDOM.src = 'dice-' + dice + '.png';
+});
 
 
 
