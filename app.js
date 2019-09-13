1986d9dd5e04c179cc2a4b283ec439d0d5a33923
zlawson13 <zach@executivedsllc.com>
2019-09-13 14:33:58 -0500

    fixes

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ function nextPlayer() {
         document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector('.player-1-panel').classList.toggle('active');
 
-        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.dice').style.display = none;
 }
 
 document.querySelector('btn-new').addEventListener('click', init);
